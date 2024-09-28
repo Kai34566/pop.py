275d69599876df1fc4fb0b660b963d17767d5c4f
Kai34566 <Ainazik.ru.kg@gmail.com>
2024-09-28 22:56:05 +0600

    Update pop.py

--- pop.py
+++ pop.py
@@ -13,7 +13,7 @@ notification_timers = {}
 
 logging.basicConfig(level=logging.INFO)
 
-bot = telebot.TeleBot("7191998889:AAHk1HXznlL0-xI7DDanbPdiYvQLI8zb_Qs")
+bot = telebot.TeleBot("7440608188:AAEAmMqPVs2vo9ijbyadX1nl9OGArvcpbzc")
 
 # Словарь со всеми чатами и игроками в этих чатах
 chat_list = {}
@@ -1458,7 +1458,7 @@ def stop_registration_timer(message):
         bot.send_message(chat_id, "⏱️ Таймер регистрации  остановлен")
     
 
-bot_username = "@nrlv_bot"
+bot_username = "@RealMafiaTestBot"
 
 def all_night_actions_taken(chat):
     for player in chat.players.values():
